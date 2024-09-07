0f16801fbadd73f1b0b509fde99df2aa61f460e9
Sawom <asawom250@gmail.com>
2024-09-08 00:11:51 +0600

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "flowbite-react";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl underline">
+      Hello world!
+      <Button>Click me</Button>
+      </h1>
+    </>
+  )
+}
+
+export default App;
