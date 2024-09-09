6f306adaa214bd7a955975dae498245644ace5f0
Sawom <asawom250@gmail.com>
2024-09-09 10:51:02 +0600

    firebase setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { Button } from "flowbite-react";
 function App() {
 
   return (
-    <>
+    <div>
       <h1 className="text-4xl underline">
       Hello world!
       <Button>Click me</Button>
       </h1>
-    </>
+    </div>
   )
 }
 
---
+++ src/Authentication/Firebase/firebase.config.jsx
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCtGQcaswgenu4Tki_qpqKz7aK2LLxA3VU",
+  authDomain: "bcpp-194bb.firebaseapp.com",
+  projectId: "bcpp-194bb",
+  storageBucket: "bcpp-194bb.appspot.com",
+  messagingSenderId: "203365006916",
+  appId: "1:203365006916:web:9e6540083dc21d11aac611",
+  measurementId: "G-NRF7Z5G6P7"
+};
+
+export default firebaseConfig;
