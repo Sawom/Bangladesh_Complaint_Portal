af7dc0d9c582acaf3ba5e2a62f5de15d11c86cb5
Sawom <asawom250@gmail.com>
2024-09-10 11:42:50 +0600

    added

--- src/Pages/Homepage/HomeComponent/Slides.jsx
+++ src/Pages/Homepage/HomeComponent/Slides.jsx
@@ -1,35 +1,34 @@
-import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
-import img1 from '../../../assets/banner/banner1.jpg'
-import img2 from '../../../assets/banner/banner2.jpg'
-import img3 from '../../../assets/banner/banner3.jpg'
-import img4 from '../../../assets/banner/banner4.jpg'
-import img5 from '../../../assets/banner/banner5.jpg'
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import img1 from "../../../assets/banner/banner1.jpg";
+import img2 from "../../../assets/banner/banner2.jpg";
+import img3 from "../../../assets/banner/banner3.jpg";
+import img4 from "../../../assets/banner/banner4.jpg";
+import img5 from "../../../assets/banner/banner5.jpg";
 
 const Slides = () => {
-    return (
-        <div className="container mx-auto ">
-            <Carousel infiniteLoop autoPlay={true} loop={true} >
-                <div>
-                    <img src={img1} />
-                </div>
-                <div>
-                    <img src={img2} />
-                </div>
-                <div>
-                    <img src={img3} />
-                    
-                </div>
-                <div>
-                    <img src={img4} />
-                </div>
-                <div>
-                    <img src={img5} />
-                </div>
-            </Carousel>
+  return (
+    <div className="container mx-auto ">
+      <Carousel infiniteLoop autoPlay={true} loop={true}>
+        <div>
+          <img src={img1} />
         </div>
-    );
+        <div>
+          <img src={img2} />
+        </div>
+        <div>
+          <img src={img3} />
+        </div>
+        <div>
+          <img src={img4} />
+        </div>
+        <div>
+          <img src={img5} />
+        </div>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Slides;
