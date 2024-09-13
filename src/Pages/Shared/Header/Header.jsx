52485be7a6509a76f6bd4875ed126645da048a51
Sawom <asawom250@gmail.com>
2024-09-13 12:01:43 +0600

    header 1st part

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import logo from '../../../assets/logo/logo.png'
 
 const Header = () => {
     return (
         <div>
-            
+           <div className='container mx-auto' >
+                <div className="navbar">
+                    {/* nav start */}
+                    <div className="flex navbar-start">
+                        <img style={{width: '60px'}} src={logo} alt="" />
+                        <p className="font-bold normal-case lg:text-2xl md:text-2xl text-sm">Bangladesh Complaint Portal</p>
+                    </div>
+                    {/* end */}
+                    {/* <div className='navbar-end'>
+                        login
+                    </div> */}
+                </div>
+            </div> 
         </div>
     );
 };
