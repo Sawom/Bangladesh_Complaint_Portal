2714e38922718a65048dd66c843af38809ef2978
Sawom <asawom250@gmail.com>
2024-10-06 12:35:56 +0600

    updated

--- src/Pages/ManageComplain/ManageComplain.jsx
+++ src/Pages/ManageComplain/ManageComplain.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 const ManageComplain = () => {
   const [complains, setComplains] = useState([]);
@@ -42,20 +43,79 @@ const ManageComplain = () => {
     } // Prevent empty search
 
     try {
-      const response = await axios.get(`http://localhost:5000/search/${searchQuery}`);
+      const response = await axios.get(
+        `http://localhost:5000/search/${searchQuery}`
+      );
       setUsers(response.data); // Update the users state with search results
     } catch (error) {
       console.error("Error searching:", error);
     }
   };
 
-
   // Function to handle page change
   const handlePageChange = (page) => {
-      fetchComplains(page); // Fetch users for the selected page
+    fetchComplains(page); // Fetch users for the selected page
   };
 
-  return <div>manage complain</div>;
+  // delete user
+  const handleDeleteComplain = (delcomplains) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Perform Axios delete operation
+        axios
+          .delete(`http://localhost:5000/complains/${delcomplains._id}`)
+          .then((res) => {
+            if (res.data.deletedCount > 0) {
+              // Show success message
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Complain has been deleted!",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+              // refetch user
+              fetchComplains(currentPage);
+            }
+          })
+          .catch((error) => {
+            console.error("Error deleting review:", error);
+            Swal.fire({
+              position: "top-end",
+              icon: "error",
+              title: "Failed to delete User.",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          });
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{ backgroundColor: "#E5E5E5", minHeight: "70vh" }}
+      className="p-3">
+      <br />
+      <div
+        className="container mx-auto mt-4 mb-4 p-3"
+        style={{ backgroundColor: "#FFFFFF" }} >
+            <h3 className="lg:text-3xl mb-5 md:text-2xl text-xl font-bold ml-4 ">
+                Total Complains: {totalComplains}
+            </h3>
+
+      </div>
+      <br />
+    </div>
+  );
 };
 
 export default ManageComplain;
